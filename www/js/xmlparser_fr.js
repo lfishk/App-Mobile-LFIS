973fb8233e483aba0aafba5637cadb7cf20b4514
laurentabbal <laurent@abbal.com>
2015-12-20 10:27:24 +0800

    text size

--- www/js/xmlparser_fr.js
+++ www/js/xmlparser_fr.js
@@ -23,7 +23,7 @@ function xmlparser()
 				xml_content += '<a href="#a_' + i + '" id="close_' + i + '" onclick="read('+ i +')" class="btn-floating waves-effect waves-light blue-grey darken-3 close_button"><i class="tiny mdi-navigation-arrow-back"></i></a>';
 				xml_content += '<a name="a_' + i + '" class="anchor"></a>';
 				xml_content += '<div id="date_' + i + '" class="col s2 center" style="clear:both;padding-top:5px;"><i class="grey-text text-darken-1 small mdi-notification-event-note"></i><div class="grey-text" style="margin-top:-5px;font-size:70%;">' + items[i].getElementsByTagName("pubDate")[0].textContent.substring(5,10) + '</div></div>';
-				xml_content += '<div id="title_' + i + '" class="col s10 item_title"><h5><a href="#a_' + i + '" onclick="read('+ i +')" style="color:#212121;">' + items[i].getElementsByTagName("title")[0].textContent + '</a></h5><br /></div>';
+				xml_content += '<div id="title_' + i + '" class="col s10 item_title"><h5 style="font-size:98%"><a href="#a_' + i + '" onclick="read('+ i +')" style="color:#212121;">' + items[i].getElementsByTagName("title")[0].textContent + '</a></h5><br /></div>';
 				xml_content += '<div id="' + i + '" class="col s12 item_description">' + items[i].getElementsByTagName("description")[0].textContent + '</div>';
 			}
 			
