14978ddf29a7b3889c10e6e9ea8696ae2fe7b559
laurentabbal <laurent@abbal.com>
2016-11-03 22:19:05 +0800

    new rss

--- www/js/xmlparser_fr.js
+++ www/js/xmlparser_fr.js
@@ -4,7 +4,7 @@ function xmlparser()
 	var xmlhttp, xml_data, items, content, article, grid;
 	xmlhttp=new XMLHttpRequest();
 	/*xmlhttp.open("GET", "feed.xml", true);*/
-	xmlhttp.open("GET", "http://www.lfihk.net/lfi-feed/lfi-feed-fr.xml", true);
+	xmlhttp.open("GET", "https://www.fis.edu.hk/fr/news-press/rss", true);
 	xmlhttp.overrideMimeType('text/xml; charset=iso-8859-1');
 	
 	xmlhttp.onreadystatechange=function()
@@ -24,8 +24,10 @@ function xmlparser()
 				xml_content += '<a name="a_' + i + '" class="anchor"></a>';
 				xml_content += '<div id="date_' + i + '" class="col s2 center" style="clear:both;padding-top:5px;"><i class="grey-text text-darken-1 small mdi-notification-event-note"></i><div class="grey-text" style="margin-top:-5px;font-size:70%;">' + items[i].getElementsByTagName("pubDate")[0].textContent.substring(5,10) + '</div></div>';
 				xml_content += '<div id="title_' + i + '" class="col s10 item_title"><h5 style="font-size:110%"><a href="#a_' + i + '" onclick="read('+ i +')" style="color:#212121;">' + items[i].getElementsByTagName("title")[0].textContent + '</a></h5><br /></div>';
-				xml_content += '<div id="' + i + '" class="col s12 item_description">' + items[i].getElementsByTagName("description")[0].textContent + '</div>';
-			}
+				xml_content += '<div id="' + i + '" class="col s12 item_description">';
+				xml_content += items[i].getElementsByTagName("description")[0].textContent
+				xml_content += '<a href="'+ items[i].getElementsByTagName("link")[0].textContent + '" class="waves-effect waves-light btn blue darken-3">suite...</a></div>';
+				xml_content += '</div>';			}
 			
 			document.getElementById('xml_content').innerHTML=xml_content;
 		}
