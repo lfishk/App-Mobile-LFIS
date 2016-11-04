f6df707849810a2832522ae458a6ed00bf98a565
laurentabbal <laurent@abbal.com>
2016-11-05 07:29:18 +0800

    test

--- www/js/init.js
+++ www/js/init.js
@@ -2,7 +2,7 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+	$('.collapsible').collapsible();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
