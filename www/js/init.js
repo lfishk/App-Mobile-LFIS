69fd8b3551f90be49de5f3b3cd0d6e6dacb4755c
laurentabbal <laurent@abbal.com>
2016-11-05 16:27:51 +0800

    design

--- www/js/init.js
+++ www/js/init.js
@@ -9,3 +9,11 @@
 $(document).ready(function(){
     $('.collapsible').collapsible();
   });
+  
+$('.button-collapse').sideNav({
+      menuWidth: 240, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+);
--- www/js/xmlparser_fr.js
+++ www/js/xmlparser_fr.js
@@ -3,8 +3,8 @@ function xmlparser()
 {
 	var xmlhttp, xml_data, items, content, article, grid;
 	xmlhttp=new XMLHttpRequest();
-	xmlhttp.open("GET", "feed.xml", true);
-	/*xmlhttp.open("GET", "http://www.lfihk.net/lfi-feed/lfi-feed-fr.xml", true);*/
+	/*xmlhttp.open("GET", "feed.xml", true);*/
+	xmlhttp.open("GET", "http://www.lfihk.net/lfi-feed/lfi-feed-fr.xml", true);
 	xmlhttp.overrideMimeType('text/xml; charset=UTF-8');
 	
 	xmlhttp.onreadystatechange=function()
@@ -26,7 +26,7 @@ function xmlparser()
 				xml_content += '<div id="title_' + i + '" class="col s10 item_title"><h5 style="padding-top:5px;font-size:110%"><a href="#a_' + i + '" onclick="read('+ i +')" style="color:#212121;">' + items[i].getElementsByTagName("title")[0].textContent + '</a></h5><br /></div>';
 				xml_content += '<div id="' + i + '" class="col s12 item_description">';
 				xml_content += items[i].getElementsByTagName("description")[0].textContent
-				xml_content += '<a href="'+ items[i].getElementsByTagName("link")[0].textContent + '" class="waves-effect waves-light btn blue darken-3">suite...</a></div>';
+				xml_content += '<a href="'+ items[i].getElementsByTagName("link")[0].textContent + '" class="waves-effect waves-light btn light-blue darken-3">suite...</a></div>';
 				xml_content += '</div>';
 			}
 			
